5db433c648a0fca89ca11670c137f2e462e0c977
Marcelo Morales <marcelomoralesuni@gmail.com>
2020-02-23 04:34:49 -0500

    fixed issues

--- src/com/danielabdelsamed/plugin/HttpConnection.java
+++ src/com/danielabdelsamed/plugin/HttpConnection.java
@@ -2,18 +2,18 @@ package com.danielabdelsamed.plugin;
 
 import org.bukkit.entity.Player;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import static org.bukkit.Bukkit.getLogger;
 
 class Payload {
    public ArrayList<String> outOfCredits;
@@ -30,19 +30,23 @@ public class HttpConnection {
         URL url = new URL("http://api.rev-uc-2020.asq.digital/plugin/current-players");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
 
         // For POST only - START
         con.setDoOutput(true);
         OutputStream os = con.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
         ArrayList<String> ids = new ArrayList<String>();
         for(Player player: players){
             ids.add(player.getUniqueId().toString());
         }
 
         Gson gson = new Gson();
-        os.write(gson.toJson(ids).getBytes());
-        os.flush();
-        os.close();
+        String json = gson.toJson(ids);
+        getLogger().info(json);
+        osw.write(json);
+        osw.flush();
+        osw.close();
 
         int code = con.getResponseCode();
 
