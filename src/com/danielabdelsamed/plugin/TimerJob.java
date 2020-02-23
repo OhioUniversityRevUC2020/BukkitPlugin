1b2b43b268bb7b589e27067173d1c8e6143fdabe
Marcelo Morales <marcelomoralesuni@gmail.com>
2020-02-23 06:33:24 -0500

    explosion does not destroy blocks, only hurts player. Warning player about token needs fixing.

--- src/com/danielabdelsamed/plugin/TimerJob.java
+++ src/com/danielabdelsamed/plugin/TimerJob.java
@@ -1,6 +1,8 @@
 package com.danielabdelsamed.plugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -9,7 +11,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
+import static java.lang.Thread.sleep;
 import static org.bukkit.Bukkit.getLogger;
 
 public class TimerJob extends TimerTask {
@@ -30,11 +34,36 @@ public class TimerJob extends TimerTask {
                 getLogger().info("Player " + id + " is out of credits! Killing!");
                 for(Player player : players){
                     if(player.getUniqueId().toString().equals(id)) {
+//                        for (int j = 0; j <3 ; j++) {
+//                            try {
+//                                TimeUnit.SECONDS.sleep(10);
+//                                player.sendMessage(ChatColor.RED + "#Staff" + ChatColor.GREEN + "Pay your dues...");
+//                            } catch (InterruptedException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }//for
+
+//                        try {
+//                            TimeUnit.SECONDS.sleep(10);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();}
+//
+//                        player.sendMessage(ChatColor.RED + "#Staff" + ChatColor.GREEN + "You had your chance...");
+//
+//                        try {
+//                            TimeUnit.SECONDS.sleep(10);
+//                            player.sendMessage(ChatColor.RED + "#Staff" + ChatColor.GREEN + "Pay your dues...");
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+
+
                         Bukkit.getScheduler().runTask(plugin, new Runnable() {
                             @Override
                             public void run() {
+
                                 for(int i = 0; i < 100; i++){
-                                    player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
+                                    player.getWorld().createExplosion(player.getLocation().getBlockX(), player.getLocation().getBlockY(), player.getLocation().getBlockZ(), 5, false, false);
                                 }
                             }
                         });
